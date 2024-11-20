0bdd2ad93cd10235bb82c3038b69114a40f799cd
eolouiszz <ilouiszk@gmail.com>
2024-11-20 20:50:28 -0300

    projeto basico de js so pra praticar:)

---
+++ main.js
@@ -0,0 +1,11 @@
+const DivGreen = document.getElementById('green')
+const DivRed = document.getElementById('red')
+const DivBlue = document.getElementById('blue')
+
+DivRed.addEventListener('click', function() {
+  alert('botão clicado')
+})
+
+function Alert() {
+  alert('botão clicado')
+}
